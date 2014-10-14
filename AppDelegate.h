6092073200f4d8e3c166efc192e58f4f68035397
rich3d <richg3d@gmail.com>
2014-10-14 09:55:12 -0700

    edit 10/14
    edited

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PickerWithDocumentsTest
+//
+//  Created by MacBook_RG on 9/14/14.
+//  Copyright (c) 2014 Richard Grice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PickerWithDocumentsTestTests/PickerWithDocumentsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  PickerWithDocumentsTestTests.m
+//  PickerWithDocumentsTestTests
+//
+//  Created by MacBook_RG on 9/14/14.
+//  Copyright (c) 2014 Richard Grice. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickerWithDocumentsTestTests : XCTestCase
+
+@end
+
+@implementation PickerWithDocumentsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
